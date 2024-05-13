2a8b54ddea3c0e0113c541cca67df047a39c706f
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2024-05-13 12:24:16 -0300

    Adiciona nova classe TexBuffer

---
+++ projeto/texbuffer.py
@@ -0,0 +1,30 @@
+from OpenGL.GL import *
+import numpy as np
+
+class TexBuffer():
+  def __init__ (self, varname, array):
+    self.varname = varname
+    self.buffer = glGenBuffers(1)
+    self.tex = glGenTextures(1)
+    self.__setData__(array)
+
+    def __set_data__ (self, array):
+        self.format = GL_RGBA32F
+        glBindTexture(GL_TEXTURE_BUFFER,self.tex)
+        glBindBuffer(GL_TEXTURE_BUFFER,self.buffer)
+        glBufferData(GL_TEXTURE_BUFFER,array.nbytes,array,GL_STATIC_DRAW)
+        glTexBuffer(GL_TEXTURE_BUFFER,self.format,self.buffer)
+    
+    def get_tex_id (self):
+        return self.tex
+
+# métodos vindos do exemplo. Entender e adaptar
+#  
+    # def load (self, st):
+    #     shd = st.GetShader()
+    #     shd.ActiveTexture(self.varname)
+    #     glBindTexture(GL_TEXTURE_BUFFER,self.tex)
+
+    # def unload (self, st):
+    #     shd = st.GetShader()
+    #     shd.DeactiveTexture()
